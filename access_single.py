922d2d1e86823a69a9416a7d6383e0c3a724d141
Victor Bai <victorbai@Victors-MacBook-Pro.local>
2016-04-20 11:15:26 -0400

    use main.py for single/range access

---
+++ access_single.py
@@ -0,0 +1,89 @@
+import sys
+import os
+
+def access_single(chr, target):
+
+	if target <= 0:
+		print "Invalid target coordinate.";
+		sys.exit();
+
+	#Decompress:
+	os.system("bzip2 -d -k ./var/chr"+chr+"_formatted.txt.bz2");
+
+	#Read variations:
+	cfile = open("./var/chr"+chr+"_formatted.txt",'r');
+	tList = cfile.readline().strip();
+	pList = cfile.readline().strip().split();
+	sList = cfile.readline().strip().split();
+	cfile.close();
+
+	#Pointer at reference genome:
+	refLoc = target;
+	#Difference used to adjust relative distance:
+	prevDiff = 0;
+	found = False;
+
+
+
+
+
+	for i in range(len(tList)):
+
+		t = tList[i];
+		p = int(pList[i]);
+		s = sList[i];
+
+		#Update the relative position
+		p = p + prevDiff;
+
+		#If access point is before next variation:
+		if target < p:
+			get_ref(chr, refLoc - 1);
+			found = True;
+			break;
+
+		#If SNP:
+		if t == '0':
+			target = target - p;
+			prevDiff = 0;
+			if target == 0:
+				found = True;
+				sys.stdout.write(s);
+				break;
+
+		#If deletion:
+		if t == '1':
+			numDel = int(s);
+			refLoc = refLoc + numDel;
+			target = target - p + 1;
+			prevDiff = 1 - numDel;
+
+		#If insertion:
+		if t == '2':
+			numIns = len(s);
+			if target <= p + numIns - 1:
+				sys.stdout.write(s[target - p]);
+				found = True;
+				break;
+			
+			refLoc = refLoc - numIns;
+			target = target - p - numIns + 1;
+			prevDiff = 1;
+
+	#If no more variation:
+	if not found:
+		get_ref(chr, refLoc - 1);
+
+	sys.stdout.write('\n');
+	os.system("rm ./var/chr"+chr+"_formatted.txt");
+
+
+def get_ref(chr, loc):
+	rfile = open("./chr/chr"+chr+".fa",'r')
+	rfile.readline();
+	n = loc / 50;
+	p = loc % 50;
+	for i in range(n):
+		rfile.readline();
+	sys.stdout.write(rfile.readline().strip()[p]);
+	rfile.close()
---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+import os
+from access_range import access_range
+from access_single import access_single
+
+#Target chromosome from user:
+chr = sys.argv[1];
+
+#Target access point from user:
+#If range acces:
+if ':' in sys.argv[2]:
+	start = int(sys.argv[2].split(':')[0]);
+	end = int(sys.argv[2].split(':')[1]);
+	if end > start:
+		access_range(chr, start, end);
+
+		#For testing:
+		# for target in range(start, end+1):
+		# 	access_single(chr, target);
+		# print ""
+	elif end == start:
+		access_single(chr, start);
+	else:
+		print "Invalid target coordinate.";
+		sys.exit();
+		
+#If single point access:
+else:
+	access_single(chr, int(sys.argv[2]));
