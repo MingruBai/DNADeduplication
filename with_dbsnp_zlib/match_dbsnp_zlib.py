358402605e77d72a2fc5b61acc147a444d944580
Victor Bai <mingru.bai@princeton.edu>
2016-05-06 11:46:27 -0400

    using zlib

---
+++ with_dbsnp_zlib/match_dbsnp_zlib.py
@@ -0,0 +1,82 @@
+import os
+import zlib
+
+chr = '1';
+ufile = open('../JWB-unified-file.txt','r');
+newufile = open('JWB-snp-file.txt','w');
+os.system("mkdir ./vector_dbsnp");
+
+while True:
+
+    print chr
+
+    vfile = open('./vector_dbsnp/vec' + chr,'w');
+    vcomp = zlib.compressobj();
+    
+    dfile = open('../with_dbsnp/dbSNP/chr' + chr +'.txt','r');
+
+    dfile.readline();
+
+    dline = dfile.readline();
+    if chr == '1':
+        uline = ufile.readline();
+    
+    while len(dline) != 0:
+
+        ulist = uline.strip().split(',');
+        t_var = int(ulist[0]);
+        c_var = ulist[1][3:];
+        i_var = int(ulist[2]);
+
+        dlist = dline.strip().split();
+        i_db = int(dlist[3]);
+
+        if t_var != 0 or c_var != chr:
+            dline = dfile.readline();
+            vfile.write(vcomp.compress('0'));
+            continue;
+
+        if i_db > i_var:
+            newufile.write(uline);
+            uline = ufile.readline();
+            continue;
+
+        if i_db < i_var:
+            dline = dfile.readline();
+            vfile.write(vcomp.compress('0'));
+            continue;
+
+        s_var = ulist[3];
+        s_db = dlist[9];
+
+        if (s_var[0] == s_db[0] and s_var[2] == s_db[2]) or (s_var[0] == s_db[2] and s_var[2] == s_db[0]):
+            vfile.write(vcomp.compress('1'));
+        else:
+            vfile.write(vcomp.compress('0'));
+            newufile.write(uline);
+
+        uline = ufile.readline();
+        dline = dfile.readline();
+
+    while t_var == 0 and c_var == chr:
+        newufile.write(uline);
+        uline = ufile.readline();
+        ulist = uline.strip().split(',');
+        t_var = int(ulist[0]);
+        c_var = ulist[1][3:];
+        i_var = int(ulist[2]);
+
+    chr = c_var
+    
+    dfile.close();
+    vfile.write(vcomp.flush());
+    vfile.close();
+
+    if t_var != 0: 
+        break;
+
+
+ufile.close();
+newufile.close();
+#os.system("bzip2 ./vector_dbsnp/*")
+
